c15e97f41c89108edc1a67d5ce902ccef7ae7287
Rudinei Costa <costaaugusto177@outlook.com>
2025-03-12 21:23:52 -0300

    first commit

--- app/index.tsx
+++ app/index.tsx
@@ -1,5 +1,7 @@
 import { Stack, Link } from 'expo-router';
 
+import LoveIcon from '../assets/love-icon.svg';
+
 import { Button } from '~/components/Button';
 import { Container } from '~/components/Container';
 import { ScreenContent } from '~/components/ScreenContent';
@@ -10,6 +12,7 @@ export default function Home() {
       <Stack.Screen options={{ title: 'Home' }} />
       <Container>
         <ScreenContent path="app/index.tsx" title="Home" />
+        <LoveIcon width={100} height={100} />
         <Link href={{ pathname: '/details', params: { name: 'Dan' } }} asChild>
           <Button title="Show Details" />
         </Link>
